3c9d2612c465f622cb39661e0930d47905d7c149
Michael Schönrock <michael.schoenrock@gmx.de>
2020-09-10 12:43:02 +0200

    Update klarna-checkout.php
    allow other plugins to use the regular woocommerce hook

--- templates/klarna-checkout.php
+++ templates/klarna-checkout.php
@@ -37,3 +37,5 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 </form>
 
 <?php do_action( 'kco_wc_after_checkout_form' ); ?>
+
+<?php do_action( 'woocommerce_after_checkout_form', $checkout ); // allow other plugins to use the regular woocommerce hook ?>
